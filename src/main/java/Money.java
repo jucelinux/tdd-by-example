662a5cbe3106dfda1356ce942a5c2bedefd2359f
jucelinux <jucelino.qjr@gmail.com>
2020-08-14 20:48:02 -0300

    Finished gutting subclasses and deleted them
    Eliminated tests that made sense with the old code structure but were redundant with the new code structure

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,29 +1,35 @@
-abstract class Money {
+public class Money {
 
     protected int amount;
     protected String currency;
-
-    abstract Money times(int multiplier);
 
     public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    public static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
     public String currency() {
         return currency;
     }
 
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(object.getClass());
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    public String toString() {
+        return amount + " " + currency;
     }
 }
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -5,14 +5,14 @@ public class DollarTest {
 
     @Test
     void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     void testFrancMultiplication() {
-        Franc five = Money.franc(5);
+        Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
@@ -22,9 +22,7 @@ public class DollarTest {
 
         assertTrue((Money.dollar(5)).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue((Money.franc(5)).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+        assertFalse((Money.franc(5)).equals(Money.dollar(5)));
 
     }
 }
