4b44984e2b37d8a949e2582bfe4ff686114a3511
jucelinux <jucelino.qjr@gmail.com>
2020-08-14 20:01:03 -0300

    Decoupling Dollar and Franc subclasses from client code

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,9 +1,19 @@
-public class Money {
+abstract class Money {
+
+    abstract Money times(int multiplier);
 
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(object.getClass());
     }
 }
